f8e1589532560fee6c98a4e625454cfb95901bb3
Ali Ismail <2220005098@iau.edu.sa>
2023-01-28 19:15:09 +0300

    Add stat file output

--- file.cpp
+++ file.cpp
@@ -53,5 +53,37 @@ void save_file(std::vector<user> &users){
 }
 
 void output_stat_file(std::vector<user> &users){
-    return;
+    // will output number of records, average age, average score, and the highest score / oldest person.
+    // will also output the time of the last update
+    std::fstream file;
+    file.open("stats.txt",std::ios::trunc | std::ios::out); // open file in trunc mode to clear the file before writing
+    file << "Number of records: " << users.size() << std::endl;
+    
+    int sum_age = 0;
+    int sum_score = 0;
+    for(int i = 0; i < users.size(); i++){
+        sum_age += users[i].age;
+        sum_score += users[i].score;
+    }
+    file << "Average age: " << sum_age / users.size() << std::endl;
+    file << "Average score: " << sum_score / users.size() << std::endl;
+
+    int max_age = 0;
+    int max_score = 0;
+    for(int i = 0; i < users.size(); i++){
+        if(users[i].age > max_age){
+            max_age = users[i].age;
+        }
+        if(users[i].score > max_score){
+            max_score = users[i].score;
+        }
+    }
+    file << "Oldest person: " << max_age << std::endl;
+    file << "Highest score: " << max_score << std::endl;
+
+    time_t loctime = time(0); // get the current time, its in UNIX timestamp
+    char readabletime[26]; 
+    strftime(readabletime, 26, "%Y-%m-%d %I:%M:%S %p", localtime(&loctime)); // format the time to our format, %I is for 12 hour clock, %p is for AM/PM
+    file << "Last update: " << readabletime << std::endl;
+    file.close();
 }
